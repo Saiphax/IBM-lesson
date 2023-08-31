bfc996349754dc2be0faa5f313fc100fa2115dd1
Saiphax <fs.sahil93@gmail.com>
2023-08-31 17:35:03 +0200

    first draft of app file

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Welcome to Sahil's App </h1>
+      <hr>
+      </hr>
+      <h3> July 25, 2023 </h3>
+    </div>
+  );
+}
+
+export default App;
